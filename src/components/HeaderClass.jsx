45c894c6076c320313631f7c7519afe319f8dcfb
小堇 <huanlei@ky27.onexmail.com>
2024-11-13 16:22:50 +0800

    学习React的第一天 原创

---
+++ src/components/HeaderClass.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+class HeaderClass extends Component{
+  render(){
+    return (
+      <h1>Hello World</h1>
+    )
+  }
+}
+export default HeaderClass;
---
+++ src/components/MainFun.jsx
@@ -0,0 +1,7 @@
+function MainFun(){
+  return (
+    <h1>Main What can I say</h1>
+  )
+}
+
+export default MainFun;
